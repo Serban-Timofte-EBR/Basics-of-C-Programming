e312a8de03c371d9c46cb64a9844218481332e92
Timofte Serban Gabriel <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2023-03-23 12:11:24 +0200

    Update ATPWeek4-1Relativ.c

--- ATPWeek4-1Relativ.c
+++ ATPWeek4-1Relativ.c
@@ -1,81 +1,73 @@
 //Creare fisier organizat relativ
 //De la tastatura introduce elemente (populare)
 //Scriem elemente in fisier in functie de elementul cod care reprezinta pozitia elementului din fisier
-#include <stdio.h>
-
-typedef struct {
+#include<stdio.h>
+typedef struct{
 	char denumire[20];
 	float pret;
-	int cant[20];
-	char is; //valori de 0 sau 1
-} Produs;
+	int cant[12];
+	char is;
+} PRODUS;
 
-int nrart(FILE* f, int l) // l este sizeof(Produs)
+int nrart(FILE *f, int l)
 {
-	long p;
-	int n;
-
-	p = ftell(f); // retine pozitia curenta din fisier
-	fseek(f, 0, 2); // pozitionare la sf fisierul
-	n = ftell(f) / l; // calculam numar articole din fisier
-	fseek(f, p, 0); // revenire la pozitia initiala
-
-	return n;
+  long p; 
+  int n;
+  p=ftell(f); 
+  fseek(f,0,2); 
+  n=ftell(f)/l;
+  fseek(f,p,0);
+  return n;
 }
 
-
 int main()
-{
-	FILE* f;
-	Produs p;
-	int i, cod; //cod ajuta la pozitionarea articolelor in fisier (unicitatea articolului)
-	char nume[20];
+{ FILE *f;
+  char nume[20]; 
+  PRODUS p; 
+  int i, cod;
 
-	f = fopen("FisRel.dat", "wb+"); //fopen_s(&f, "FisRel.dat", "wb+");
+  printf("\nFisier: ");
+  gets(nume);
+  f=fopen(nume,"wb+");  
+ //deschide pentru citire si scriere. daca fis exista, este suprascris, altfel este creat
+ 
+  printf("\n Cod produs: ");
+  scanf("%d",&cod); 
+  while(!feof(stdin))
+  {
+     if(cod>=nrart(f,sizeof(PRODUS)))
+           {
+            p.is=0; 
+            fseek(f,0,2);
+            for(i=nrart(f,sizeof(PRODUS));i<=cod;i++)
+            fwrite(&p,sizeof(PRODUS), 1, f);
+           }
+     fseek(f,cod*sizeof(PRODUS),0); 
+     fread(&p,sizeof(PRODUS), 1, f);
 
-	printf("Cod produs: ");
-	scanf("%d", &cod);
-
-	while (!feof(stdin))
-	{
-		if (cod >= nrart(f, sizeof(Produs)))
-		{
-			//Preformare fisier pentru a ajunde la pozitia data de cod
-			p.is = 0;
-			fseek(f, 0, 2);
-			for (i = nrart(f, sizeof(Produs)); i <= cod; i++)
-			{
-				fwrite(&p, sizeof(Produs), 1, f);
-				fseek(f, cod * sizeof(Produs), 0); //pozitionare la inceputul articolul scris
-				fread(&p, sizeof(Produs), 1, f);
-
-				if (p.is == 1) { printf("Articolul exista deja!"); }
-				else {
-					fseek(f, cod * sizeof(Produs), 0);
-					printf("Denumire: ");
-					fflush(stdin);
-					scanf("%s", &p.denumire);
-
-					printf("Pret: ");
-					scanf("%f", &p.pret);
-
-					p.is = 1;
-
-					for (i = 0; i < 3; i++)
-					{
-						printf("Cant[%d] = ", i + 1);
-						scanf("%d", &p.cant[i]);
-					}
-
-					p.is = 1; //marcare articol scris
-
-					fwrite(&p, sizeof(Produs), 1, f); //p - sursa si f destinatia
-				}
-			}
-			printf("Cod produs NOU: ");
-			scanf("%d", &cod);
-		}
-	}
-	fclose(f);
-	return 0;
+      if(p.is) printf("\nExista deja un produs cu acest cod");
+      else
+          {
+            fseek (f,  cod*sizeof(PRODUS),  0);
+            printf("Denumire: "); 
+            fflush(stdin);
+            gets(p.denumire); 
+           
+            printf("pret:"); 
+            scanf("%f",&p.pret);
+           
+            for(i=0;i<12;i++) 
+         	 {
+              	             printf("Cant %d: ",i+1); 
+              	             scanf("%d",&p.cant[i]);
+                 }
+          
+              p.is=1; 
+              fwrite(&p,  sizeof(PRODUS),   1,   f);
+             }
+   
+       printf("\nCod nou: "); 
+       scanf("%d",&cod);
+   }
+fclose(f);
 }
